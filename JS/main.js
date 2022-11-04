f36954686264de9d7117ec73a2b8780b43f26d5d
FilippoBid <filippoperrone1997@gmail.com>
2022-11-04 17:44:14 +0100

    fine esercizio base da fixare misure

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,33 @@
 const buttonPrev = document.getElementById ("prev");
 const buttonNext = document.getElementById ("next");
 const imgEl = document.querySelector(".bgcardimg")
-const imgs = ["img/01.webp", "img/02.webp", "img/03.webp", "img/04.webp", "img/05.webp"]; 
-imgEl.src =
+const imgs = ["img/01.webp", "img/02.webp", "img/03.webp", "img/04.webp", "img/05.webp"];
+let imgCount = 0; 
+imgEl.src = imgs[imgCount]
+console.log(imgs.length);
+buttonNext.addEventListener("click", function(){
+    if (imgCount == (imgs.length - 1)){
+        imgCount = 0;
+        imgEl.src = imgs[imgCount]
+
+
+    }else{ 
+        imgCount = imgCount + 1
+        imgEl.src = imgs[imgCount]
+    }
+    
+   
+
+    
+});
+buttonPrev.addEventListener("click", function(){
+    if(imgCount == 0){
+        imgCount = imgs.length - 1
+        imgEl.src = imgs[imgCount]
+    }else{
+        imgCount = imgCount - 1
+        imgEl.src = imgs[imgCount]
+    }
+
+     
+});
