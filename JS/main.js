9d74a495f650c496a00da055a28b12e9ebac5335
FilippoBid <filippoperrone1997@gmail.com>
2022-11-04 17:00:13 +0100

    inizio il progetto

---
+++ JS/main.js
@@ -0,0 +1,5 @@
+const buttonPrev = document.getElementById ("prev");
+const buttonNext = document.getElementById ("next");
+const imgEl = document.querySelector(".bgcardimg")
+const imgs = ["img/01.webp", "img/02.webp", "img/03.webp", "img/04.webp", "img/05.webp"]; 
+imgEl.src =
